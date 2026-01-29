141634e8d299348c433f6b2f42c2abad402aca45
Farhan Ahmad <me.farhanahmad@gmail.com>
2026-01-29 14:02:56 +0100

    update plugins and libs

--- app/src/main/java/com/black/chessclock/MainActivity.kt
+++ app/src/main/java/com/black/chessclock/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
-import androidx.compose.ui.tooling.preview.Preview
 import com.black.chessclock.ui.theme.ChessClockTheme
 
 class MainActivity : ComponentActivity() {
